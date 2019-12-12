9dbc50582ac5772b9f1740e79156a50c0ff6cbb9
Kevin van der Werff <Kevin van der Werff>
2019-12-12 01:49:25 +0100

    day 2 part 2

--- day-02/index.js
+++ day-02/index.js
@@ -28,20 +28,56 @@ const performOperation = (startingIndex, opcode, list) => {
 }
 
 const run = list => {
-  let opcode = list[0];
+  const copy = [...list];
+  let opcode = copy[0];
   let currentIndex = 0;
 
-  while (opcode != 99) {
-    performOperation(currentIndex, opcode, list);
+  while (opcode !== 99) {
+    performOperation(currentIndex, opcode, copy);
     currentIndex += 4;
-    opcode = list[currentIndex];
+    opcode = copy[currentIndex];
   }
 
-  return list;
+  return copy;
 }
 
 // Restore 1202 program alarm state
 inputData[1] = 12;
 inputData[2] = 2;
 
+
+// "Tests"
+const deepEquality = (list1, list2) => {
+  if (list1.length !== list2.length) return false;
+  
+  for (let i = 0; i < list1.length; i++) {
+    if (list1[i] !== list2[i]) return false; 
+  }
+
+  return true;
+}
+
+console.log(deepEquality(run([1,0,0,0,99]), [2,0,0,0,99]))
+console.log(deepEquality(run([2,3,0,3,99]), [2,3,0,6,99]))
+console.log(deepEquality(run([2,4,4,5,99,0]), [2,4,4,5,99,9801]))
+console.log(deepEquality(run([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99]))
+
 console.log(run(inputData)[0]); // -> 5110675
+
+/// Part 02
+
+const find19690720 = list => {
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const copy = [...list];
+      copy[1] = noun;
+      copy[2] = verb;
+      const updated = run(copy);
+      if (updated[0] === 19690720) { return [noun, verb] }
+    }
+  }
+  return [-1, -1];
+} 
+
+const [noun, verb] = find19690720(inputData);
+console.log(100 * noun + verb); // -> 4847
