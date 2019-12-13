acdfda919107ec6855e416a7f9a5b2c1976e0948
Kevin van der Werff <Kevin van der Werff>
2019-12-13 01:42:47 +0100

    commit attempt day 3 part 1

--- day-03/index.js
+++ day-03/index.js
@@ -1,11 +1,63 @@
 import { } from 'ramda';
 
 /// Part 1
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getTuple() { return [this.x, this.y]; }
+}
+
+class Line {
+  constructor(point1, point2) {
+    this.point1 = point1;
+    this.point2 = point2;
+  }
+
+  getTuple() { return [this.point1, this.point2]; }
+}
+
+// getSlope :: Line -> Int
+const getSlope = line => {
+  const [point1, point2] = line.getTuple();
+  const [x1, y1] = point1.getTuple();
+  const [x2, y2] = point2.getTuple();
+  return (y2 - y1) / (x2 - x1);
+}
+
+const getYIntercept = (point, slope) => point.y - slope * point.x;
+
+// doIntersect :: Line -> Line -> Bool
+const doIntersect = (line1, line2) => {
+  const slope1 = getSlope(line1);
+  const yIntercept1 = getYIntercept(line1, slope1);
+  const slope2 = getSlope(line2);
+  const yIntercept2 = getYIntercept(line2, slope2);
+
+  console.log('============');
+  console.log(slope1);
+  console.log(slope2);
+  // If slope is equal, they don't intersect
+  if (slope1 - slope2 === 0) return false;
+
+  // Assume they intersect (thus, x1 === x2 && y1 === y2)
+  const x = (yIntercept2 - yIntercept1) / (slope1 - slope2)
+  const hypoY1 = slope1 * x + yIntercept1;
+  const hypoY2 = slope2 * x + yIntercept2;
+  console.log(x);
+  console.log(hypoY1);
+  console.log(hypoY2);
+
+  return hypoY1 === hypoY2;
+}
 
 const intersectionPoint = () => {
+  const intersectionPoints = [];
 
 }
 
 /// Part 2
 
-export { intersectionPoint }
+export { getSlope, getYIntercept, intersectionPoint, doIntersect, Point, Line }
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,10 +1,40 @@
 import fs from 'fs';
-import { } from 'ramda';
-import { intersectionPoint } from './index';
+import { map, filter, reduce, split } from 'ramda';
+import { getSlope, getYIntercept, intersectionPoint, Point, Line, doIntersect } from './index';
 
 // Read in data and clean it
+const splitWires = split('\n');
+const splitInstructions = split(',');
+
 const txt = fs.readFileSync('./day-03/input.txt', 'utf8');
-const inputData; // Create input data as appropriate
+const inputData = map(splitInstructions, splitWires(txt));
+
+test('Slope', () => {
+  const point1 = new Point(1, 1);
+  const point2 = new Point(3, 3);
+  const line1 = new Line(point1, point2);
+  expect(getSlope(line1)).toEqual(1);
+  // expect(getSlope([[1, 3], [5, 9]])).toEqual(1.5);
+  // expect(getSlope([[10, 10], [15, 5]])).toEqual(-1);
+});
+
+test('Y-intercept', () => {
+  const point1 = new Point(1, 1);
+  const slope = 1;
+  expect(getYIntercept(point1, slope)).toEqual(0);
+  // expect(getYIntercept([[1, 3], [5, 9]])).toEqual(1.5);
+  // expect(getYIntercept([[10, 10], [15, 5]])).toEqual(20);
+});
+
+test('doIntersect', () => {
+  const point1 = new Point(2, 0);
+  const point2 = new Point(2, 8);
+  const line1 = new Line(point1, point2);
+  const point3 = new Point(1, 1);
+  const point4 = new Point(4, 1);
+  const line2 = new Point(point3, point4);
+  expect(doIntersect(line1, line2)).toEqual(true);
+})
 
 test('Pass the examples for part 1', () => {
   expect(intersectionPoint(['R75','D30','R83','U83','L12','D49','R71','U7','L72'], ['R75','D30','R83','U83','L12','D49','R71','U7','L72'])).toEqual(159);
