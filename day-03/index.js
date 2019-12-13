18fbaafda84ea127aa239280b14d1e60c9dde58e
Kevin Van Der Werff <Kevinsebastianate@hotmail.com>
2019-12-13 18:47:26 +0100

    day 3 part 1

--- day-03/index.js
+++ day-03/index.js
@@ -2,6 +2,7 @@ import { map, reduce, splitAt, uniq, min } from 'ramda';
 
 /// Part 1
 // Point :: { x, y } with x & y ints
+// Line :: [Point, Point]
 
 const DIRECTIONS = {
   U: ({x, y}) => ({ x, y: y + 1 }),
@@ -10,36 +11,119 @@ const DIRECTIONS = {
   L: ({x, y}) => ({ x: x - 1, y }),
 }
 
-// generatePoints :: [instructions] -> [Point]
-const generatePoints = instructions => {
-  const points = reduce((acc, instruction) => {
-    const [direction, amount] = splitAt(1, instruction);
-    const op = DIRECTIONS[direction];
-    
-    let lastPoint = acc[acc.length - 1];
-    for (let i = 0; i < amount; i++) {
-      const newPoint = op(lastPoint);
-      acc.push(newPoint);
-      lastPoint = newPoint;
-    }
-    return acc
-  }, [{ x: 0, y: 0 }], instructions);
-  
-  return uniq(points);
+// xIntersection :: Line -> Point -> Bool
+const xProjectsOnLine = (point, [point1, point2]) =>
+  (point1.x <= point.x && point.x <= point2.x) ||
+  (point2.x <= point.x && point.x <= point1.x);
+
+// yIntersection :: Line -> Point -> Bool
+const yProjectsOnLine = (point, [point1, point2]) =>
+  (point1.y <= point.y && point.y <= point2.y) ||
+  (point2.y <= point.y && point.y <= point1.y);
+
+// mightIntersect :: Line -> Line -> Bool
+const mightIntersect = (line1, line2) => {
+  const [point1, point2] = line1;
+  const [point3, point4] = line2;
+
+  return (
+    xProjectsOnLine(point1, line2) ||
+    xProjectsOnLine(point2, line2) ||
+    xProjectsOnLine(point3, line1) ||
+    xProjectsOnLine(point4, line1)
+  ) && (
+    yProjectsOnLine(point1, line2) ||
+    yProjectsOnLine(point2, line2) ||
+    yProjectsOnLine(point3, line1) ||
+    yProjectsOnLine(point4, line1)
+  )
 }
 
+// manhattenDistance :: Point -> Int
 const manhattenDistance = ({ x, y }) => Math.abs(x) + Math.abs(y);
 
-const intersectionPoint = (instructionsWire1, instructionsWire2) => {
-  const points1 = generatePoints(instructionsWire1);
-  const points2 = generatePoints(instructionsWire2);
+// turningPoints :: [Instruction] -> [Point]
+const turningPoints = instructions => {
+  return reduce((acc, instruction) => {
+    const [direction, amount] = splitAt(1, instruction);
+    const op = DIRECTIONS[direction];
 
-  const sharedPoints = points1.filter((point => points2.some(({ x, y }) => x === point.x && y === point.y )));
-  sharedPoints.shift(); // Remove { x: 0, y: 0 }
-  const manhattenDistances = map(manhattenDistance, sharedPoints);
+    let currentPoint = acc[acc.length - 1];
+    for (let i = 0; i < amount; i++) {
+      currentPoint = op(currentPoint);
+    }
+    acc.push(currentPoint);
+    return acc;
+  }, [{ x: 0, y: 0 }], instructions);
+}
+
+// direction :: Line -> Char
+const direction = ([point1, point2]) => {
+  let instruction;
+  if (point1.x < point2.x) {
+    instruction = 'R'
+  } else if (point1.x > point2.x) {
+    instruction = 'L'
+  } else if (point1.y < point2.y) {
+    instruction = 'U'
+  } else {
+    instruction = 'D'
+  }
+  return instruction;
+}
+
+// allPoints :: Line -> [Point]
+const allPoints = line => {
+  const directionLine = direction(line);
+  const [startingPoint, endingPoint] = line;
+  
+  const allPoints = [startingPoint];
+  let currentPoint = startingPoint;
+
+  while (currentPoint.x !== endingPoint.x || currentPoint.y !== endingPoint.y) {
+    const op = DIRECTIONS[directionLine];
+    currentPoint = op(currentPoint);
+    allPoints.push(currentPoint);
+  }
+
+  return allPoints;
+}
+
+// commonPoints :: Line -> Line -> [Point]
+const commonPoints = (line1, line2) => {
+  const pointsLine1 = allPoints(line1);
+  const pointsLine2 = allPoints(line2);
+  return pointsLine1.filter(point => pointsLine2.some(point2 => point.x === point2.x && point.y === point2.y));
+}
+
+// distanceClosestCommonPoint :: [Instruction] -> [Instruction] -> Int
+const distanceClosestCommonPoint = (instructionsWire1, instructionsWire2) => {
+  let turningPointsWire1 = turningPoints(instructionsWire1);
+  let turningPointsWire2 = turningPoints(instructionsWire2);
+
+  let allCommonPoints = [];
+  for (let i = 0; i < turningPointsWire1.length - 1; i++) {
+    const point1 = turningPointsWire1[i];
+    const point2 = turningPointsWire1[i + 1];
+
+    for (let j = 0; j < turningPointsWire2.length - 1; j ++) {
+      const point3 = turningPointsWire2[j];
+      const point4 = turningPointsWire2[j + 1];
+
+      const line1 = [point1, point2];
+      const line2 = [point3, point4];
+
+      if (mightIntersect(line1, line2)) {
+        allCommonPoints = [...allCommonPoints, ...commonPoints(line1, line2)];
+      }
+    }
+  }
+
+  allCommonPoints.shift(); // Remove { x: 0; y: 0 }
+  const manhattenDistances = map(manhattenDistance, allCommonPoints);
   return reduce(min, Infinity, manhattenDistances);
 }
 
 /// Part 2
 
-export { generatePoints, intersectionPoint }
+export { distanceClosestCommonPoint, mightIntersect }
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { map, filter, reduce, split } from 'ramda';
-import { generatePoints, intersectionPoint } from './index';
+import { distanceClosestCommonPoint, mightIntersect } from './index';
 
 // Read in data and clean it
 const splitWires = split('\n');
@@ -9,29 +9,23 @@ const splitInstructions = split(',');
 const txt = fs.readFileSync('./day-03/input.txt', 'utf8');
 const inputData = map(splitInstructions, splitWires(txt));
 
-test('generatePoints', () => {
-  expect(generatePoints(['R1', 'D1', 'L1', 'U1'])).toEqual([{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: -1 }, { x: 0, y: -1 }]);
-  expect(generatePoints(['R5','D3','R8','U1','L0','D1','R2','U3','L2'])).toEqual([
-    { x: 0, y: 0 },
-    /* R5 */ { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 }, { x: 5, y: 0 },
-    /* D3 */ { x: 5, y: -1 }, { x: 5, y: -2 }, { x: 5, y: -3 },
-    /* R8 */ { x: 6, y: -3 }, { x: 7, y: -3 }, { x: 8, y: -3 }, { x: 9, y: -3 }, { x: 10, y: -3 }, { x: 11, y: -3 }, { x: 12, y: -3 }, { x: 13, y: -3 },
-    /* U1 */ { x: 13, y: -2 },
-    /* L0 */
-    /* D1 Duplicated coordinate removed */
-    /* R2 */ { x: 14, y: -3 }, { x: 15, y: -3 },
-    /* U3 */ { x: 15, y: -2 }, { x: 15, y: -1 }, { x: 15, y: 0 },
-    /* L2 */ { x: 14, y: 0 }, { x: 13, y: 0 }
-  ]);
+test('mightIntersect', () => {
+  const line1 = [{ x: 1, y: 1 }, { x: 3, y: 3 }];
+  const line2 = [{ x: 2, y: 1 }, { x: 2, y: 5 }];
+  expect(mightIntersect(line1, line2)).toEqual(true);
+
+  const line3 = [{ x: 2, y: 2 }, { x: 3, y: 3 }];
+  const line4 = [{ x: 0, y: 0 }, { x: 1, y: 1 }];
+  expect(mightIntersect(line3, line4)).toEqual(false);
 });
 
 test('Pass the examples for part 1', () => {
-  expect(intersectionPoint(['R75','D30','R83','U83','L12','D49','R71','U7','L72'], ['U62','R66','U55','R34','D71','R55','D58','R83'])).toEqual(159);
-  expect(intersectionPoint(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'], ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])).toEqual(135);
+  expect(distanceClosestCommonPoint(['R75','D30','R83','U83','L12','D49','R71','U7','L72'], ['U62','R66','U55','R34','D71','R55','D58','R83'])).toEqual(159);
+  expect(distanceClosestCommonPoint(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'], ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])).toEqual(135);
 });
 
 test('Pass the puzzle input for part 1', () => {
-  expect(intersectionPoint(inputData[0], inputData[1])).toEqual(0);
+  expect(distanceClosestCommonPoint(inputData[0], inputData[1])).toEqual(2180);
 });
 
 test('Pass the examples for part 2', () => {
