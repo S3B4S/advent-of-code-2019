9d48153bfb607676f1819e27435462a57d80be70
Kevin Van Der Werff <Kevinsebastianate@hotmail.com>
2019-12-15 00:33:58 +0100

    pass examples day 6 part 1 attempt 1

--- day-06/index.js
+++ day-06/index.js
@@ -1,7 +1,54 @@
-import { } from 'ramda';
+import { split, compose, reduce, map, isEmpty } from 'ramda';
 
-/// Part 1
+/*
+* Node = {
+*   id: Int,
+*   parent: Node,
+*   children: [Node],
+* }
+*/
+const addNode = (id, nodes) => {
+  return nodes.set(id, {
+    id,
+    parentId: '-1',
+    childrenIds: [],
+  });
+}
 
-/// Part 2
+const addRelation = (parentId, childId, nodes) => {
+  const parentNode = nodes.get(parentId);
+  const childNode = nodes.get(childId);
+  const newParent = {
+    ...parentNode,
+    childrenIds: [childId, ...parentNode.childrenIds],
+  }
+  const newChild = {
+    ...childNode,
+    parentId,
+  }
+  nodes.set(childId, newChild);
+  nodes.set(parentId, newParent);
+  return nodes;
+}
 
-export { }
+const countOrbitsOfNode = (node, nodes) => {
+  if (node.parentId === '-1') return 0;
+  return 1 + countOrbitsOfNode(nodes.get(node.parentId), nodes);
+}
+
+const countOrbits = tree => reduce((total, node) => total + countOrbitsOfNode(node, tree), 0, tree.values());
+
+const createNodes = reduce((acc, [parentId, childId]) => {
+  if (!acc.has(parentId)) { acc = addNode(parentId, acc); }
+  if (!acc.has(childId)) { acc = addNode(childId, acc); }
+  acc = addRelation(parentId, childId, acc);
+  return acc;
+}, new Map())
+
+const entry1 = compose(
+  countOrbits,
+  createNodes,
+  map(split(')'))
+)
+
+export { entry1 }
--- day-06/index.test.js
+++ day-06/index.test.js
@@ -1,15 +1,32 @@
 import fs from 'fs';
-import { } from 'ramda';
-import { } from './index';
+import { split, isEmpty, filter, not, compose } from 'ramda';
+import { entry1 } from './index';
 
 // Read in data and clean it
 const txt = fs.readFileSync('./day-06/input.txt', 'utf8');
-const inputData; // Create input data as appropriate
+const cleanData = compose(
+  filter(compose(not, isEmpty)),
+  split('\n'),
+)
+const inputData = cleanData(txt);
 
 test('Pass the examples for part 1', () => {
+  expect(entry1(cleanData(
+    "COM)B\n" +
+    "B)C\n" +
+    "C)D\n" +
+    "D)E\n" +
+    "E)F\n" +
+    "B)G\n" +
+    "G)H\n" +
+    "D)I\n" +
+    "E)J\n" +
+    "J)K\n" +
+    "K)L"))).toEqual(42);
 });
 
 test('Pass the puzzle input for part 1', () => {
+  expect(entry1(inputData)).toEqual(0)
 });
 
 test('Pass the examples for part 2', () => {
