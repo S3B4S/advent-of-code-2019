40fb61bac075bff2907bc5a92a5be8794d6f7f01
Kevin Van Der Werff <Kevinsebastianate@hotmail.com>
2019-12-15 20:49:57 +0100

    pass day 5.1

--- day-05/index.js
+++ day-05/index.js
@@ -1,30 +1,54 @@
-import { add, multiply, toString, reverse, map, curry } from 'ramda';
+import { add, multiply, toString, reverse, map, curry, zip, isNil } from 'ramda';
 
 const mapWithIndex = curry((fn, list) => list.map(fn));
 
 let USER_INPUT = 0;
 let OUTPUT = 0;
 
+const resolveValue = (list, [mode, parameter]) => {
+  if (mode === PARAMETER_MODE.immediate) {
+    return parameter;
+  } else if (mode === PARAMETER_MODE.position) {
+    return OPERATIONS['READ'].fn(list, parameter)
+  }
+}
+
 // [function, incrementPointerBy]
 const OPERATIONS = {
   '01': {
     // Add and store at address
-    fn: curry((list, a, b, address) => { list[address] = a + b }),
+    fn: curry((list, aMode, bMode, addressMode) => {
+      const a = resolveValue(list, aMode);
+      const b = resolveValue(list, bMode);
+      const [_, address] = addressMode;
+      list[address] = a + b;
+    }),
     incrementPointerBy: 4,
   },
   '02': {
     // Multiply and store at address
-    fn: curry((list, a, b, address) => { list[address] = a * b }),
+    fn: curry((list, aMode, bMode, addressMode) => {
+      const a = resolveValue(list, aMode);
+      const b = resolveValue(list, bMode);
+      const [_, address] = addressMode;
+      list[address] = a * b;
+    }),
     incrementPointerBy: 4,
   },
   '03': {
-    // Write input to address
-    fn: curry((list, address) => { list[address] = USER_INPUT }),
+    // Write user input to address
+    fn: curry((list, addressMode) => {
+      const [_, address] = addressMode;
+      list[address] = USER_INPUT;
+    }),
     incrementPointerBy: 2,
   },
   '04': {
     // Read from address and outputs it
-    fn: curry((list, address) => { OUTPUT = list[address] }),
+    fn: curry((list, addressMode) => {
+      const [_, address] = addressMode;
+      OUTPUT = list[address];
+    }),
     incrementPointerBy: 2,
   },
   'READ': {
@@ -38,47 +62,40 @@ const PARAMETER_MODE = {
   immediate: '1', // parameter is the value itself
 }
 
-const performOperation = ([opcode, ...modes], startPointer, list) => {
+const performOperation = (opcode, modes, startPointer, list) => {
   const op = OPERATIONS[opcode].fn(list);
-  
-  const values = mapWithIndex((mode, i) => {
-    const currentPointer = startPointer + 1 + i;
-    const parameter = list[currentPointer];
-    if (mode === PARAMETER_MODE.immediate || i === 2 || opcode === '03' || opcode === '04') {
-      return parameter
-    } else if (mode === PARAMETER_MODE.position) {
-      return OPERATIONS['READ'].fn(list)(parameter)
-    }
-  }, modes);
-
-  op(...values);
+  const parameters = [1, 2, 3].map(x => {
+    return list[startPointer + x];
+  })
+  const parametersAndModes = zip(modes, parameters);
+  op(...parametersAndModes);
 }
 
-const parseInstruction = opcode => {
-  const chars = toString(opcode);
-  if (chars.length === 1) return ['0' + chars, '0'];
-
-  // Beyond this an instruction has in between 2-5 chars
+const parseInstruction = instruction => {
+  let chars = toString(instruction);
+  // Beyond this an instruction has in between 1-5 chars
   // missing chars are leading zeros
-  const [instructionChar1, instructionChar2, firstMode, secondMode, thirdMode] = reverse(chars);
-  const temp = [instructionChar2 + instructionChar1, firstMode, secondMode, thirdMode];
-  return map(char => !char ? '0' : char, temp);
+  while (chars.length !== 5) {
+    chars = '0' + chars;
+  }
+  const [third, second, first, ...opcode] = chars;
+  return [opcode.join(''), [first, second, third]]
 }
 
 const run = (list, input) => {  
   USER_INPUT = input;
   const copy = [...list];
   let instruction = copy[0];
-  let currentIndex = 0;
+  let currentPointer = 0;
 
   while (instruction !== 99) {
-    const instructions = parseInstruction(instruction)
-    performOperation(instructions, currentIndex, copy);
-    currentIndex += OPERATIONS[instructions[0]].incrementPointerBy;
-    instruction = OPERATIONS['READ'].fn(copy, currentIndex);
+    const [opcode, modes] = parseInstruction(instruction);
+    performOperation(opcode, modes, currentPointer, copy);
+    currentPointer += OPERATIONS[opcode].incrementPointerBy;
+    instruction = OPERATIONS['READ'].fn(copy, currentPointer);
   }
 
-  return copy;
+  return OUTPUT;
 }
 
 export { run };
--- day-05/index.test.js
+++ day-05/index.test.js
@@ -7,14 +7,8 @@ const isNotNaN = compose(not, isNaN);
 const txt = fs.readFileSync('./day-05/input.txt', 'utf8');
 const inputData = filter(isNotNaN, map(parseInt, split(',', txt)));
 
-// console.log(inputData);
-
-test('Pass the examples for part 1', () => {
-  console.log(run([1002,4,3,4,33], 1));
-  console.log(run([3,0,4,0,99], 1));
-});
-
 test('Pass the puzzle input for part 1', () => {
+  expect(run(inputData, 1)).toEqual(15314507);
 });
 
 test('Pass the examples for part 2', () => {
