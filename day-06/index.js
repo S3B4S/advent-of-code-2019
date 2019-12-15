4fc95b217f98eabaebbc3e037574caea687a4932
Kevin Van Der Werff <Kevinsebastianate@hotmail.com>
2019-12-15 18:32:37 +0100

    attempt plain object instead of Map

--- day-06/index.js
+++ day-06/index.js
@@ -8,34 +8,32 @@ import { split, compose, reduce, map, isEmpty, tap } from 'ramda';
 * }
 */
 const addNode = (id, nodes) => {
-  nodes.set(id, {
+  nodes[id] = {
     id,
     parent: {},
-    children: [],
-  });
+  };
 }
 
 const addEdge = (parentId, childId, nodes) => {
-  const parentNode = nodes.get(parentId);
-  const childNode = nodes.get(childId);
+  const parentNode = nodes[parentId];
+  const childNode = nodes[childId];
 
-  parentNode.children = [childNode, ...parentNode.children];
   childNode.parent = parentNode;
 };
 
 const createTree = reduce((tree, [parentId, childId]) => {
-  if (!tree.has(parentId)) { addNode(parentId, tree); }
-  if (!tree.has(childId)) { addNode(childId, tree); }
+  if (!tree[parentId]) { addNode(parentId, tree); }
+  if (!tree[childId]) { addNode(childId, tree); }
   addEdge(parentId, childId, tree);
   return tree;
-}, new Map());
+}, {});
 
 const countOrbitsOfNode = node => {
   if (isEmpty(node.parent)) return 0;
   return 1 + countOrbitsOfNode(node.parent);
 };
 
-const countOrbitsOfTree = tree => reduce((total, node) => total + countOrbitsOfNode(node), 0, tree.values());
+const countOrbitsOfTree = tree => reduce((total, node) => total + countOrbitsOfNode(node), 0, Object.values(tree));
 
 const totalOrbits = compose(
   countOrbitsOfTree,
